2970c468414842c88559f4ffa7867cc0f4e760a9
Yudhi <baskyudh@gmail.com>
2025-03-07 14:13:10 +0700

    Add imports and event listeners to nav menu - Add clearContent function - Each button now corresponds to their respective content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import "./styles.css";
+import * as home from "./home.js";
+import * as menu from "./menu.js";
+import * as contact from "./contact.js";
 
 console.log("is working");
+const navMenu = document.querySelector("nav");
+
+home.loadHome();
+
+const content = document.querySelector("#content");
+
+const clearContent = () => {
+  content.innerHTML = "";
+}
+
+navMenu.addEventListener("click", e => {
+  if (e.target.classList.contains("home")){
+    clearContent();
+    home.loadHome();
+  } else if (e.target.classList.contains("menu")) {
+    clearContent();
+    menu.loadMenu();
+  } else if (e.target.classList.contains("contact")) {
+    clearContent();
+    contact.loadContact();
+  }
+})
+

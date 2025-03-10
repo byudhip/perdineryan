91120e889055aef461877770d93582f5c48e9963
Yudhi <baskyudh@gmail.com>
2025-03-10 20:39:09 +0700

    Move content variable initialization - Function was previously not working

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,8 @@
-const content = document.querySelector("#content");
-
-
 const loadContact = () => {
-  const headingText = document.createElement("h1");
-  headingText.textContent = "This is contact page";
-  content.appendChild(headingText);
+  const content = document.querySelector("#content");
+  const heading = document.createElement("h1");
+  heading.textContent = "This is contact page";
+  content.appendChild(heading);
 }
 
 export {loadContact};

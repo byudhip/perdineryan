6be95029675bec1fba81dc1775cc7a1d1a077213
Yudhi <baskyudh@gmail.com>
2025-03-11 10:22:20 +0700

    Move button handler functions to utils.js - Add id as parameter to CreateElement() - Add default values to CreateElement() parameter

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import "./styles.css";
 import * as home from "./home.js";
-import * as menu from "./menu.js";
-import * as contact from "./contact.js";
+import * as utils from "./utils.js";
 
 home.initHome();  
-
-const content = document.querySelector("#content");
-console.log("is working");
 const navMenu = document.querySelector("nav");
+navMenu.addEventListener("click", utils.buttonHandler);
 
-const clearContent = () => {
-  content.innerHTML = "";
-}
-
-navMenu.addEventListener("click", e => {
-  if (e.target.classList.contains("home")){
-    clearContent();
-    home.loadHome();
-  } else if (e.target.classList.contains("menu")) {
-    clearContent();
-    menu.loadMenu();
-  } else if (e.target.classList.contains("contact")) {
-    clearContent();
-    contact.loadContact();
-  }
-})
 
--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,31 @@
+import * as home from "./home.js";
+import * as menu from "./menu.js";
+import * as contact from "./contact.js";
+
 function getImages(r) {
   let images = {};
-  r.keys().forEach(key =>{
-    images[key.replace("./","")] = r(key);
+  r.keys().forEach((key) => {
+    images[key.replace("./", "")] = r(key);
   });
   return images;
 }
 
-function CreateElement(tag, className, textContent) {
+function CreateElement(tag, id = null, className = null, textContent = null) {
   const element = document.createElement(tag);
+  if (id) element.id = id;
   if (className) element.classList.add(className);
   if (textContent) element.textContent = textContent;
   return element;
 }
 
-export {getImages, CreateElement};
+function buttonHandler(e) {
+  if (e.target.classList.contains("home")) {
+    home.loadHome();
+  } else if (e.target.classList.contains("menu")) {
+    menu.loadMenu();
+  } else if (e.target.classList.contains("contact")) {
+    contact.loadContact();
+  }
+}
+
+export { getImages, CreateElement, buttonHandler };

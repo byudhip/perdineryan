a12f6fa1d28c778a03be29c7f4f99595a150b771
Yudhi <baskyudh@gmail.com>
2025-03-11 10:20:06 +0700

    Add built-in clear innerHTML of #content

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,6 @@
 const loadContact = () => {
   const content = document.querySelector("#content");
+  content.innerHTML = "";
   const heading = document.createElement("h1");
   heading.textContent = "This is contact page";
   content.appendChild(heading);
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,6 @@
 const loadMenu = () => {
   const content = document.querySelector("#content");
+  content.innerHTML = "";
   const heading = document.createElement("h1");
   heading.textContent = "This is menu page";
   content.appendChild(heading);
